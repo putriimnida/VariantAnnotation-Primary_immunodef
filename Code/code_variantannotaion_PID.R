7953251d508812e53e6c09b5bc55cb36915936db
Putri Ramadani <putrira2112@gmail.com>
2025-02-13 14:33:16 -0500

    updated code

--- Code/code_variantannotaion_PID.R
+++ Code/code_variantannotaion_PID.R
@@ -1,1 +1,10 @@
 # variant annotation on primary immunodeficiency
+
+# Load the necessary library
+library(biomaRt)
+
+# Load gene list
+genes <- read.delim("Primary immunodeficiency or monogenic inflammatory bowel disease.tsv", sep = "\t", header = TRUE)
+
+# check column names
+colnames(genes)
